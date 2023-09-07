6b9e2d436e13d868ba7a89c316d71c2f17e1cc2a
Mathieu H <mathieuhatin@hotmail.com>
2023-09-07 14:21:56 -0400

    firebase version Web et Android fonctionne

--- lib/main_firebase.dart
+++ lib/main_firebase.dart
@@ -1,13 +1,44 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 
-Future main() async{
+
+// Future main() async{
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+//   runApp(MyApp());
+// }
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  
+  if (kIsWeb) {
+    print("Flutter Web");    
+    // Votre logique spécifique pour le Web ici
+    const FirebaseOptions webOption = FirebaseOptions(
+      apiKey: "AIzaSyA4u2Z2oiqbOuyNpYWdWcGSqyNDhuK6xFY",
+      authDomain: "exercices-flutter.firebaseapp.com",
+      projectId: "exercices-flutter",
+      storageBucket: "exercices-flutter.appspot.com",
+      messagingSenderId: "399288472806",
+      appId: "1:399288472806:web:a36ebd644c191217831727",
+      measurementId: "G-N4YWPVQQCJ"
+    );
+    await Firebase.initializeApp(
+      options: webOption,
+    );
+  } else if (defaultTargetPlatform == TargetPlatform.android) {
+    print("Flutter Android");
+    // Votre logique spécifique pour Android ici
+     await Firebase.initializeApp();
+  }
+ 
   runApp(MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   @override
